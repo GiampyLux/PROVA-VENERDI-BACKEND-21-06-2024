be549ad6b6a0e78d62812196c62ac7ef4efdbffe
GiampyLux <giampaolopaladino01@gmail.com>
2024-06-21 11:35:01 +0200

    Aggiungere i file di progetto.

---
+++ CalcoloImposta/Contribuente.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcoloImposta
+{
+    internal class Contribuente
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public DateTime DataDiNascita { get; set; }
+        public string CodiceFiscale { get; set; }
+        public string Sesso { get; set; }
+        public string Comune { get; set; }
+        public decimal RedditoAnnuale { get; set; }
+
+        public Contribuente(string nome, string cognome, DateTime datadinascita, string comune, string codiceFiscale, string sesso, decimal redditoAnnuale)
+        {
+            Nome = nome;
+            Cognome = cognome;
+            DataDiNascita = datadinascita;
+            CodiceFiscale = codiceFiscale;
+            Sesso = sesso;
+            Comune = comune;
+            RedditoAnnuale = redditoAnnuale;
+        }
+
+        public decimal Calcolo()
+        {
+            decimal imposta = 0;
+            decimal reddito = RedditoAnnuale;
+
+            if (reddito < 15000)
+            {
+                imposta = reddito * 0.23m;
+            }
+            else if (reddito <= 28000) 
+            { 
+                imposta = 3450 + (reddito - 15000) * 0.27m; 
+
+            }
+            else if (reddito <= 55000)
+            {
+                imposta = 6960 + (reddito - 28000) * 0.38m;
+            }
+            else if (reddito <= 75000)
+            {
+                imposta = 17220 + (reddito - 55000) * 0.41m;
+            }
+            else
+            {
+                imposta = 25420 + (reddito - 75000) * 0.43m;
+            }
+
+            return imposta;
+
+
+
+
+        }
+    }
+}
---
+++ CalcoloImposta/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace CalcoloImposta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserisci nome");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Inserisci Cognome");
+            string cognome = Console.ReadLine();
+            Console.WriteLine("Insercisi la tua data di nascita");
+            DateTime datadinascita = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Inserisci il codice fiscale:");
+            string codiceFiscale = Console.ReadLine();
+
+            Console.WriteLine("Inserisci il tuo Genere");
+            string sesso = Console.ReadLine();
+
+            Console.WriteLine("Inserisci il comune");
+            string comune = Console.ReadLine();
+
+            Console.WriteLine("Inserisci il reddito");
+            decimal reddito = decimal.Parse(Console.ReadLine());
+
+
+            Contribuente contribuente = new Contribuente(nome, cognome, datadinascita, comune, codiceFiscale, sesso, reddito);
+
+            decimal impostaDaVers = contribuente.Calcolo();
+            Console.WriteLine("==================================================");
+            Console.WriteLine("CALCOLO DELL'IMPOSTA DA VERSARE:");
+            Console.WriteLine($"Contribuente: {contribuente.Nome} {contribuente.Cognome},");
+            Console.WriteLine($"nato il {contribuente.DataDiNascita:dd/MM/yyyy} ({contribuente.Sesso}),");
+            Console.WriteLine($"residente in {contribuente.Comune},");
+            Console.WriteLine($"codice fiscale: {contribuente.CodiceFiscale}");
+            Console.WriteLine($"Reddito dichiarato: {contribuente.RedditoAnnuale}");
+            Console.WriteLine($"IMPOSTA DA VERSARE: {impostaDaVers}");
+        }
+    }
+}
